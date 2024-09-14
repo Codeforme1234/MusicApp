29bf8d96f522b7421cd08c1b5d3690048916533d
codeforme <deveshtiwari2511@gmail.com>
2024-09-14 13:16:35 +0530

    playlist height fixed

--- src/app/components/Playlist.tsx
+++ src/app/components/Playlist.tsx
@@ -75,7 +75,7 @@ const Playlist: React.FC<PlaylistProp> = () => {
         </div>
 
         <div className="overflow-scroll no-scrollbar overflow-x-hidden mt-2">
-          <div className="flex flex-col text-white md:h-[200px]">
+          <div className="flex flex-col text-white h-[40vh] md:h-[200px]">
             {reversedPlaylist.map((song: Song, index: number) => (
               <MusicCard
                 key={index}
@@ -95,7 +95,7 @@ const Playlist: React.FC<PlaylistProp> = () => {
       </div>
 
       <div className="overflow-scroll no-scrollbar overflow-x-hidden text-white">
-        <div className="flex flex-col text-white md:h-[240px] mt-2">
+        <div className="flex flex-col text-white h-[50vh] md:h-[240px] mt-2">
           {songs.map((song, index) => (
             <MusicCard
               key={index}
