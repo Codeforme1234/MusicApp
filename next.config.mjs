dad6e5a399553549a5e1a94024c7a5b9ce3529fa
codeforme <deveshtiwari2511@gmail.com>
2024-10-07 12:18:12 +0530

    images set to unoptimised

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['atwoodmagazine.com','media.licdn.com','picsum.photos','cdn.hearthis.at','img.hearthis.at','cdn.pixabay.com', 'thatgrapejuice.net','www.dafont.com','blowyaspeakers.com','headlineplanet.com','is1-ssl.mzstatic.com','i.scdn.co','s.mxmcdn.net','i.ytimg.com','p1.music.126.net','images2.imgbox.com'], 
-    },
-  };
+  images: {
+    domains: [
+      "atwoodmagazine.com",
+      "media.licdn.com",
+      "picsum.photos",
+      "cdn.hearthis.at",
+      "img.hearthis.at",
+      "cdn.pixabay.com",
+      "thatgrapejuice.net",
+      "www.dafont.com",
+      "blowyaspeakers.com",
+      "headlineplanet.com",
+      "is1-ssl.mzstatic.com",
+      "i.scdn.co",
+      "s.mxmcdn.net",
+      "i.ytimg.com",
+      "p1.music.126.net",
+      "images2.imgbox.com",
+    ],
+    unoptimized: true,
+  },
+};
 
-  export default nextConfig;
+export default nextConfig;
