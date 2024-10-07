f865d7857fa5b41f53b1e0463f6621c5e597ee63
codeforme <deveshtiwari2511@gmail.com>
2024-10-07 13:29:13 +0530

    set Default song

--- src/app/state/SongAtom.ts
+++ src/app/state/SongAtom.ts
@@ -5,8 +5,13 @@ import { Song, SongState } from "../Utils/interfaces";
 export const songState = atom<SongState>({
   key: "songState",
   default: {
-    currentSong: null,
+    currentSong: {
+      artist: "Blues is the Truth",
+      image:
+        "https://img.hearthis.at/1/7/2/_/uploads/9657207/image_track/11416089/w500_q70_----1728073086145.jpg",
+      title: "Blues is the Truth 724",
+      url: "https://hearthis.app/ian-mchugh/blues-is-the-truth-724/listen/?s=FUc",
+    },
     playlist: [],
   },
 });
-
